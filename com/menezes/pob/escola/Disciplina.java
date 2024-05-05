9f6cd142469aff85aafc8c4b49c57e9a086ef74b
Pandore1O <143713423+Pandore1O@users.noreply.github.com>
2024-05-05 20:10:08 -0300

    Re-upload das primeiras classes

---
+++ com/menezes/pob/escola/Disciplina.java
@@ -0,0 +1,49 @@
+package com.menezes.pob.escola;
+
+public class Disciplina {
+	
+	private String nome;
+	private String sigla;
+	private String cargaHoraria;
+	private String semestre;
+	
+	public Disciplina(String nome, String sigla, String cargaHoraria, String semestre) {
+		super();
+		this.nome = nome;
+		this.sigla = sigla;
+		this.cargaHoraria = cargaHoraria;
+		this.semestre = semestre;
+	}
+	
+	public String getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public void setCargaHoraria(String cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+	
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+	public String getSemestre() {
+		return semestre;
+	}
+	
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+}
